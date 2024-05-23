225885577c1dd51f3a7acff5dcc25ee2a444fce2
OXsatoshi <afia.nabil24@gmail.com>
2024-05-23 13:57:49 +0100

    Implement transition feature

--- src/sideBare.js
+++ src/sideBare.js
@@ -1,51 +1,58 @@
-import './styles.css';
-import allTesks from './img/allTasks.svg';
-import addCercle from './img/plus-circle.svg'
-import {renderBoxWithTextIcon} from './boxWithTextandIcon';
-import {renderNewProjectUi} from './newProjectAdder';        
-import { lock } from './lock';
-const sideBareContent = [{text:"All Tasks",icon:allTesks,id:"all-tasks"},{text:"today",icon:allTesks,id:"today-task"},{text:"Next 7 Day Task",icon:allTesks,icon:allTesks,id:'week-task'},
-    {text:"Important",icon:allTesks,id:"important-task"}
+import "./styles.css";
+import allTesks from "./img/allTasks.svg";
+import addCercle from "./img/plus-circle.svg";
+import { renderBoxWithTextIcon } from "./boxWithTextandIcon";
+import { renderNewProjectUi } from "./newProjectAdder";
+import { lock } from "./lock";
+const sideBareContent = [
+  { text: "All Tasks", icon: allTesks, id: "all-tasks" },
+  { text: "today", icon: allTesks, id: "today-task" },
+  { text: "Next 7 Day Task", icon: allTesks, icon: allTesks, id: "week-task" },
+  { text: "Important", icon: allTesks, id: "important-task" },
 ];
 const sideBareDiv = document.querySelector(".side-bare");
-function handleAddingProject(){
-    if(!lock){
-        lock = true;
-        const div = renderNewProjectUi();
- document.querySelector('.project-box').insertBefore(div,document.querySelector('#add-project'));
-    }
- 
+function handleAddingProject() {
+  if (!lock) {
+    lock = true;
+    const div = renderNewProjectUi();
+    document
+
+      .querySelector(".project-box")
+      .insertBefore(div, document.querySelector("#add-project"));
+  }
 }
-function displayHomecontent(){
-    const homeDiv = document.createElement('div');
-    const homeTile = document.createElement('h1');
-    homeTile.textContent = "Home";
-    homeDiv.appendChild(homeTile);
-    homeDiv.classList.add('home-box');
+function displayHomecontent() {
+  const homeDiv = document.createElement("div");
+  const homeTile = document.createElement("h1");
+  homeTile.textContent = "Home";
+  homeDiv.appendChild(homeTile);
+  homeDiv.classList.add("home-box");
 
-    sideBareContent.forEach(e=>{homeDiv.appendChild(renderBoxWithTextIcon(e.text,e.icon,e.id))});
-    sideBareDiv.appendChild(homeDiv); 
+  sideBareContent.forEach((e) => {
+    homeDiv.appendChild(renderBoxWithTextIcon(e.text, e.icon, e.id));
+  });
+  sideBareDiv.appendChild(homeDiv);
 }
-function displayProjectContent(){
-    const projectDiv = document.createElement('div');
-    projectDiv.classList.add('project-box');
-    const projectTitle = document.createElement('h1');
-    projectTitle.textContent = "Project";
-    projectDiv.appendChild(projectTitle);
-    const addProject = renderBoxWithTextIcon("Add Project",addCercle,"add-project");
-    projectDiv.appendChild(addProject);
-    sideBareDiv.appendChild(projectDiv);
+function displayProjectContent() {
+  const projectDiv = document.createElement("div");
+  projectDiv.classList.add("project-box");
+  const projectTitle = document.createElement("h1");
+  projectTitle.textContent = "Project";
+  projectDiv.appendChild(projectTitle);
+  const addProject = renderBoxWithTextIcon(
+    "Add Project",
+    addCercle,
+    "add-project"
+  );
+  projectDiv.appendChild(addProject);
+  sideBareDiv.appendChild(projectDiv);
 
-    addProject.addEventListener("click",handleAddingProject);
-
+  addProject.addEventListener("click", handleAddingProject);
 }
-function displaySideMenu(){
-    displayHomecontent();
-    displayProjectContent();
-   console.log('side-menu');
+function displaySideMenu() {
+  displayHomecontent();
+  displayProjectContent();
+  console.log("side-menu");
 }
 
-
-
-
-export {displaySideMenu};
+export { displaySideMenu };
--- src/themeChangeHandler.js
+++ src/themeChangeHandler.js
@@ -1,31 +1,39 @@
-import { darkMode ,lightMode,menuIconForWhite,menuIconForDark} from './iconImageGenerator';
-import './styles.css';
+import {
+  darkMode,
+  lightMode,
+  menuIconForWhite,
+  menuIconForDark,
+} from "./iconImageGenerator";
+import "./styles.css";
 
 const rootStyles = getComputedStyle(document.documentElement);
-const whiteThmeColor = rootStyles.getPropertyValue('--white-theme');
-const darkThemeColor = rootStyles.getPropertyValue('--dark-theme');
-const themeSwitcher = document.querySelector('.theme-switcher');
-const cercle = document.querySelector('.cercle');
-function handleLightTheme(inPageLoad){
-            cercle.classList.remove('moved');
-            if(inPageLoad){
-                themeSwitcher.removeChild(lightMode);
-            document.querySelector(".left-side").removeChild(menuIconForDark);
+const whiteThmeColor = rootStyles.getPropertyValue("--white-theme");
+const darkThemeColor = rootStyles.getPropertyValue("--dark-theme");
+const themeSwitcher = document.querySelector(".theme-switcher");
+const cercle = document.querySelector(".cercle");
+function handleLightTheme(inPageLoad) {
+  cercle.classList.remove("moved");
+  if (inPageLoad) {
+    themeSwitcher.removeChild(lightMode);
+    document.querySelector(".left-side").removeChild(menuIconForDark);
+  } else {
+  }
+  themeSwitcher.appendChild(darkMode);
 
-            }
-            else{}
-            themeSwitcher.appendChild(darkMode);
-            
-            document.querySelector('body').style.backgroundColor = whiteThmeColor;
-            document.querySelector('body').style.color = rootStyles.getPropertyValue('--light-theme-font-color');
-}   
-function handleDarkTheme(){
-    cercle.classList.add('moved');
-    document.querySelector('body').style.backgroundColor = darkThemeColor;
-    document.querySelector(".left-side").removeChild(menuIconForWhite);
-    document.querySelector(".left-side").appendChild(menuIconForDark);
-    themeSwitcher.appendChild(lightMode);
-
-    document.querySelector('body').style.color = rootStyles.getPropertyValue('--dark-theme-font-color');
+  document.querySelector("body").style.backgroundColor = whiteThmeColor;
+  document.querySelector("body").style.color = rootStyles.getPropertyValue(
+    "--light-theme-font-color"
+  );
 }
-export {handleLightTheme,handleDarkTheme};
+function handleDarkTheme() {
+  cercle.classList.add("moved");
+  document.querySelector("body").style.backgroundColor = darkThemeColor;
+  document.querySelector(".left-side").removeChild(menuIconForWhite);
+  document.querySelector(".left-side").appendChild(menuIconForDark);
+  themeSwitcher.appendChild(lightMode);
+
+  document.querySelector("body").style.color = rootStyles.getPropertyValue(
+    "--dark-theme-font-color"
+  );
+}
+export { handleLightTheme, handleDarkTheme };
